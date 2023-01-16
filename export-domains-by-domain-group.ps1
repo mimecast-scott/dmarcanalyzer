e3db393421969ee56598d3ecb2a35ca2a4122e7e
Scott <113950533+mimecast-scott@users.noreply.github.com>
2023-01-16 11:56:54 +1100

    Update export-domains-by-domain-group.ps1

--- export-domains-by-domain-group.ps1
+++ export-domains-by-domain-group.ps1
@@ -1,6 +1,9 @@
 # This script will output a CSV file of all domain groups on an account with each domain. 
 
-Note: ```/customers/{customer}/domaingroup``` will not work, as domain groups are filters that include wildcards. 1 domain can belong to many domain groups.
+Note: 
+````
+/customers/{customer}/domaingroup````
+ will not work, as domain groups are filters that include wildcards. 1 domain can belong to many domain groups.
 
 $token = "<--api-token-->"
 $custid = "<--customer-id-->"
