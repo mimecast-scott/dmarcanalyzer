7adc2fe493790c0d1ff37d01e7eb864e5cd2d1af
Scott <113950533+mimecast-scott@users.noreply.github.com>
2023-12-13 13:58:31 +1100

    Update update-domain-group.ps1

--- update-domain-group.ps1
+++ update-domain-group.ps1
@@ -1,1 +1,7 @@
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", "Bearer <-- your bearer token -->")
 
+# NOTE: domain_patterns needs to be an array and can be represented by appending &domain_patterns[x] to the end of the Uri where x is the array object id (incremented by 1)
+
+$response = Invoke-RestMethod 'https://api.dmarcanalyzer.com/customers/<--customer-id->>/domaingroup/<--group-ip-->>?name=Finance&activity=all&domain_patterns[0]=*.example.org&domain_patterns[1]=example.org' -Method 'PUT' -Headers $headers
+$response | ConvertTo-Json
